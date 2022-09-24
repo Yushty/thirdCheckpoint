d23737a462ba7c5d7bd3448d1e250f9998fbbc5c
Aïcha Tidjani <aishamoustaphatidji@gmail.com>
2022-09-24 21:13:58 +0000

    first commit

---
+++ thirdcheckpoint.py
@@ -0,0 +1,27 @@
+def list_multiply():
+     l_mult = [2,3,6]
+     for x in range([2,3,6]):
+        l_mult.append(x*l_mult)
+        print(list_multiply)
+
+def sorted():  
+    sorted(tuple, key=las_ele)
+    new_lis=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+    las_ele = sorted(new_lis, key=lambda i: i[-1])
+    print(las_ele)
+
+
+def combine():
+    d1 = {'a', 'b', 'c'}
+    d2 = {'a', 'b', 'd'}
+    d1.update(d2)
+    print(d1)
+
+def dictionnary():
+    dico = {"pinson": 2, "merle": 3}
+    for cle, valeur in dico.items():
+        print(cle, valeur)
+
+def tuple():
+    tupl = [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]
+    print(sorted)
